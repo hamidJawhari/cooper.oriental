6a62ac5ecb2b7ec0b5272d0d8a4bc777a04c5498
hamidJawhari <abd.samad.3576@gmail.com>
2021-06-20 12:52:47 +0000

    added a new slider to website

--- js/script.js
+++ js/script.js
@@ -3,6 +3,45 @@ window.onload = function() {
   document.body.removeChild(load);
   document.querySelector(".site").style.display = "block";
 }
+/* slid */
+const myslide = document.querySelectorAll('.myslide');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 3000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 3000);
+}
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+}
+/* end slid */
 let openList  = document.getElementById("open");
 let closeList = document.getElementById("close");
 let menu      = document.querySelector(".links");
