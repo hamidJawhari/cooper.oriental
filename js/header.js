2bc51a0e1f8911093db149a2794034558c238a88
hamidJawhari <abd.samad.3576@gmail.com>
2021-06-20 12:51:16 +0000

    added  new pages like stock and shipping

---
+++ js/header.js
@@ -0,0 +1,49 @@
+function fixedNav() {
+  let nav = document.querySelector("header");
+  let links = document.querySelectorAll(".header-content ul li a");
+  if(document.documentElement.scrollTop > nav.getBoundingClientRect().height){
+    nav.classList.add("fixed-nav");
+    links.forEach(link => {
+      link.classList.add("hover-a")
+    })
+  }else {
+    nav.classList.remove("fixed-nav");
+    links.forEach(link => {
+      link.classList.remove("hover-a")
+    })
+  }
+}
+onscroll = function(){
+  fixedNav();
+}
+let openList  = document.getElementById("open");
+let closeList = document.getElementById("close");
+let menu      = document.querySelector(".links");
+let li        = document.querySelector(".links li")
+openList.addEventListener("click", function(){
+  menu.classList.add("show-list");
+  li.classList.add("li-style");
+  openList.classList.toggle("hide");
+  closeList.classList.toggle("show");
+})
+closeList.addEventListener("click", function(){
+  menu.classList.remove("show-list");
+  openList.classList.toggle("hide");
+  closeList.classList.toggle("show");
+})
+window.onresize = function() {
+  let width = window.innerWidth;
+  if(width > 768) {
+    menu.classList.remove("show-list");
+    openList.classList.remove("hide");
+    closeList.classList.remove("show");
+  }
+}
+let links =  document.querySelectorAll(".links li");
+links.forEach(e => {
+  e.addEventListener("click", function(){
+    menu.classList.remove("show-list");
+    closeList.classList.remove("show");
+    openList.classList.remove("hide");
+  })
+})
--- js/script.js
+++ js/script.js
@@ -41,57 +41,13 @@ function slidefun(n) {
 	   }
 	myslide[counter - 1].style.display = "block";
 }
+document.querySelector(".next").addEventListener("click",function(){
+  plusSlides(1);
+})
+document.querySelector(".prev").addEventListener("click",function(){
+  plusSlides(-1);
+})
 /* end slid */
-let openList  = document.getElementById("open");
-let closeList = document.getElementById("close");
-let menu      = document.querySelector(".links");
-let li        = document.querySelector(".links li")
-openList.addEventListener("click", function(){
-  menu.classList.add("show-list");
-  li.classList.add("li-style");
-  openList.classList.toggle("hide");
-  closeList.classList.toggle("show");
-})
-closeList.addEventListener("click", function(){
-  menu.classList.remove("show-list");
-  openList.classList.toggle("hide");
-  closeList.classList.toggle("show");
-})
-window.onresize = function() {
-  let width = window.innerWidth;
-  if(width > 768) {
-    menu.classList.remove("show-list");
-    openList.classList.remove("hide");
-    closeList.classList.remove("show");
-  }
-}
-let links =  document.querySelectorAll(".links li");
-links.forEach(e => {
-  e.addEventListener("click", function(){
-    menu.classList.remove("show-list");
-    closeList.classList.remove("show");
-    openList.classList.remove("hide");
-  })
-})
-onscroll = function() {
-  fixedNav();
-  showItem();
-}
-function fixedNav() {
-  let nav = document.querySelector("header");
-  let links = document.querySelectorAll(".header-content ul li a");
-  if(document.documentElement.scrollTop > nav.getBoundingClientRect().height){
-    nav.classList.add("fixed-nav");
-    links.forEach(link => {
-      link.classList.add("hover-a")
-    })
-  }else {
-    nav.classList.remove("fixed-nav");
-    links.forEach(link => {
-      link.classList.remove("hover-a")
-    })
-  }
-}
 function showItem() {
   let contentText  = document.querySelector(".content-text");
   let contentTitle = document.querySelector(".content-title");
@@ -99,4 +55,8 @@ function showItem() {
     contentTitle.classList.add("fade-in");
     contentText.classList.add("fade-in");
   }
+}
+onscroll = function() {
+  showItem();
+  fixedNav()
 }
