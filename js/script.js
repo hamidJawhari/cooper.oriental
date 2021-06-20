4db3b2d3775b6ebd2f2c5be02cc0ddc24de7b6cc
hamidJawhari <abd.samad.3576@gmail.com>
2021-06-20 13:00:37 +0000

    i add splash screen on web site

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-onload = function() {
-  document.querySelector(".load").remove()
+window.onload = function() {
+  document.querySelector(".load").remove();
   document.querySelector(".site").style.display = "block";
 }
 let openList  = document.getElementById("open");
@@ -7,29 +7,29 @@ let closeList = document.getElementById("close");
 let menu      = document.querySelector(".links");
 let li        = document.querySelector(".links li")
 openList.addEventListener("click", function(){
-  menu.classList.add("show-list")
-  li.classList.add("li-style")
-  openList.classList.toggle("hide")
-  closeList.classList.toggle("show")
+  menu.classList.add("show-list");
+  li.classList.add("li-style");
+  openList.classList.toggle("hide");
+  closeList.classList.toggle("show");
 })
 closeList.addEventListener("click", function(){
-  menu.classList.remove("show-list")
-  openList.classList.toggle("hide")
-  closeList.classList.toggle("show")
+  menu.classList.remove("show-list");
+  openList.classList.toggle("hide");
+  closeList.classList.toggle("show");
 })
 
 window.onresize = function() {
   let width = window.innerWidth;
   if(width > 768) {
-    menu.classList.remove("show-list")
-    openList.classList.remove("hide")
-    closeList.classList.remove("show")
+    menu.classList.remove("show-list");
+    openList.classList.remove("hide");
+    closeList.classList.remove("show");
   }
 }
 let links =  document.querySelectorAll(".links li");
 links.forEach(e => {
   e.addEventListener("click", function(){
-    menu.classList.remove("show-list")
+    menu.classList.remove("show-list");
     closeList.classList.remove("show");
     openList.classList.remove("hide");
   })
@@ -56,7 +56,7 @@ function fixedNav() {
 function showItem() {
   let contentText  = document.querySelector(".content-text");
   let contentTitle = document.querySelector(".content-title");
-  if(contentTitle.getBoundingClientRect().top < innerHeight / 1.3){
+  if(contentTitle.getBoundingClientRect().top < innerHeight / 1.3) {
     contentTitle.classList.add("fade-in");
     contentText.classList.add("fade-in");
   }
